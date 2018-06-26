038f3217728bb967766751f696442a521576b4e5
Maxime MARTENS <contact@maximemartens.fr>
2018-06-26 22:13:17 +0200

    added modules, pinout

---
+++ include/pinconfig.hh
@@ -0,0 +1,20 @@
+#ifndef PINCONFIG_HH_
+#define PINCONFIG_HH_
+
+//Standard buttons
+#define VOLUME_UP   16
+#define FORWARD     29
+#define PLAY_PAUSE  31
+#define REWIND      33
+#define VOLUME_DN   37
+
+//Serial buses
+#define DATA        23
+#define CLOCK       24
+
+//I2S buses
+#define I2S_BCK     12
+#define I2S_DOUT    21
+#define I2S_LRCK    35
+
+#endif /* !PINCONFIG_HH_ */
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
+#include "wiringpi\wiringPi\wiringPi.h"
 
 int main(int argc, char **argv)
 {
+  wiringPiSetupGpio(); //Init GPIO
+  
   return (0);
 }
